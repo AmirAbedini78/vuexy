208ddb5c71d6efe05521ef42c13c69c0d0bb74b1
AmirHosein <abedinnezhad1999@gmail.com>
2025-07-04 17:25:45 +0330

    #light and icones

--- resources/js/plugins/vuetify/theme.js
+++ resources/js/plugins/vuetify/theme.js
@@ -1,5 +1,5 @@
-export const staticPrimaryColor = '#7367F0'
-export const staticPrimaryDarkenColor = '#675DD8'
+export const staticPrimaryColor = '#EC8D22'
+export const staticPrimaryDarkenColor = '#EC8D22'
 export const themes = {
   light: {
     dark: false,
