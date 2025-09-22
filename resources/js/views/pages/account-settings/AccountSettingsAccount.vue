c6bffd3cda3eb8b3a26e0c6ddd849077c49ebd9f
AmirHosein <abedinnezhad1999@gmail.com>
2025-09-22 09:44:03 +0330

    #debug

--- resources/js/views/pages/account-settings/AccountSettingsAccount.vue
+++ resources/js/views/pages/account-settings/AccountSettingsAccount.vue
@@ -1,126 +1,139 @@
 <script setup>
-import avatar1 from '@images/avatars/avatar-1.png'
+import avatar1 from "@images/avatars/avatar-1.png";
 
-const cookieUser = useCookie('userData')
+const cookieUser = useCookie("userData");
 const accountData = {
   avatarImg: cookieUser.value?.avatar || avatar1,
-  firstName: cookieUser.value?.name?.split(' ')[0] || '',
-  lastName: cookieUser.value?.name?.split(' ').slice(1).join(' ') || '',
-  email: cookieUser.value?.email || '',
-  org: '',
-  phone: '',
-  address: '',
-  state: '',
-  zip: '',
-  country: '',
-  language: 'English',
-  timezone: '(GMT-11:00) International Date Line West',
-  currency: 'USD',
-}
+  firstName: cookieUser.value?.name?.split(" ")[0] || "",
+  lastName: cookieUser.value?.name?.split(" ").slice(1).join(" ") || "",
+  email: cookieUser.value?.email || "",
+  org: "",
+  phone: "",
+  address: "",
+  state: "",
+  zip: "",
+  country: "",
+  language: "English",
+  timezone: "(GMT-11:00) International Date Line West",
+  currency: "USD",
+};
 
-const refInputEl = ref()
-const isConfirmDialogOpen = ref(false)
-const accountDataLocal = ref(structuredClone(accountData))
-const isAccountDeactivated = ref(false)
-const validateAccountDeactivation = [v => !!v || 'Please confirm account deactivation']
+const refInputEl = ref();
+const isConfirmDialogOpen = ref(false);
+const accountDataLocal = ref(structuredClone(accountData));
+const isAccountDeactivated = ref(false);
+const validateAccountDeactivation = [
+  (v) => !!v || "Please confirm account deactivation",
+];
 
 const resetForm = () => {
-  accountDataLocal.value = structuredClone(accountData)
-}
+  accountDataLocal.value = structuredClone(accountData);
+};
 
 const saveProfile = async () => {
   try {
-    const fullName = `${accountDataLocal.value.firstName} ${accountDataLocal.value.lastName}`.trim()
-    const payload = { name: fullName || undefined, email: accountDataLocal.value.email || undefined }
-    const res = await $api('/admin/users/' + (useCookie('userData').value?.id || ''), { method: 'PUT', body: payload })
+    const fullName =
+      `${accountDataLocal.value.firstName} ${accountDataLocal.value.lastName}`.trim();
+    const payload = {
+      name: fullName || undefined,
+      email: accountDataLocal.value.email || undefined,
+    };
+    const res = await $api(
+      "/admin/users/" + (useCookie("userData").value?.id || ""),
+      { method: "PUT", body: payload }
+    );
     if (res?.user) {
-      const u = useCookie('userData').value || {}
-      useCookie('userData').value = { ...u, name: res.user.name, email: res.user.email }
+      const u = useCookie("userData").value || {};
+      useCookie("userData").value = {
+        ...u,
+        name: res.user.name,
+        email: res.user.email,
+      };
     }
   } catch (e) {
-    console.error('Failed to save profile', e)
+    console.error("Failed to save profile", e);
   }
-}
+};
 
-const changeAvatar = async file => {
-  const { files } = file.target
+const changeAvatar = async (file) => {
+  const { files } = file.target;
   if (files && files.length) {
-    const form = new FormData()
-    form.append('avatar', files[0])
+    const form = new FormData();
+    form.append("avatar", files[0]);
     try {
-      const res = await $api('/account/avatar', { method: 'POST', body: form })
+      const res = await $api("/account/avatar", { method: "POST", body: form });
       if (res?.url) {
-        accountDataLocal.value.avatarImg = res.url
-        const u = cookieUser.value || {}
-        cookieUser.value = { ...u, avatar: res.url }
+        accountDataLocal.value.avatarImg = res.url;
+        const u = cookieUser.value || {};
+        cookieUser.value = { ...u, avatar: res.url };
       }
     } catch (e) {
-      console.error('Avatar upload failed', e)
+      console.error("Avatar upload failed", e);
     }
   }
-}
+};
 
 // reset avatar image
 const resetAvatar = () => {
-  accountDataLocal.value.avatarImg = accountData.avatarImg
-}
+  accountDataLocal.value.avatarImg = accountData.avatarImg;
+};
 
 const timezones = [
-  '(GMT-11:00) International Date Line West',
-  '(GMT-11:00) Midway Island',
-  '(GMT-10:00) Hawaii',
-  '(GMT-09:00) Alaska',
-  '(GMT-08:00) Pacific Time (US & Canada)',
-  '(GMT-08:00) Tijuana',
-  '(GMT-07:00) Arizona',
-  '(GMT-07:00) Chihuahua',
-  '(GMT-07:00) La Paz',
-  '(GMT-07:00) Mazatlan',
-  '(GMT-07:00) Mountain Time (US & Canada)',
-  '(GMT-06:00) Central America',
-  '(GMT-06:00) Central Time (US & Canada)',
-  '(GMT-06:00) Guadalajara',
-  '(GMT-06:00) Mexico City',
-  '(GMT-06:00) Monterrey',
-  '(GMT-06:00) Saskatchewan',
-  '(GMT-05:00) Bogota',
-  '(GMT-05:00) Eastern Time (US & Canada)',
-  '(GMT-05:00) Indiana (East)',
-  '(GMT-05:00) Lima',
-  '(GMT-05:00) Quito',
-  '(GMT-04:00) Atlantic Time (Canada)',
-  '(GMT-04:00) Caracas',
-  '(GMT-04:00) La Paz',
-  '(GMT-04:00) Santiago',
-  '(GMT-03:30) Newfoundland',
-  '(GMT-03:00) Brasilia',
-  '(GMT-03:00) Buenos Aires',
-  '(GMT-03:00) Georgetown',
-  '(GMT-03:00) Greenland',
-  '(GMT-02:00) Mid-Atlantic',
-  '(GMT-01:00) Azores',
-  '(GMT-01:00) Cape Verde Is.',
-  '(GMT+00:00) Casablanca',
-  '(GMT+00:00) Dublin',
-  '(GMT+00:00) Edinburgh',
-  '(GMT+00:00) Lisbon',
-  '(GMT+00:00) London',
-]
+  "(GMT-11:00) International Date Line West",
+  "(GMT-11:00) Midway Island",
+  "(GMT-10:00) Hawaii",
+  "(GMT-09:00) Alaska",
+  "(GMT-08:00) Pacific Time (US & Canada)",
+  "(GMT-08:00) Tijuana",
+  "(GMT-07:00) Arizona",
+  "(GMT-07:00) Chihuahua",
+  "(GMT-07:00) La Paz",
+  "(GMT-07:00) Mazatlan",
+  "(GMT-07:00) Mountain Time (US & Canada)",
+  "(GMT-06:00) Central America",
+  "(GMT-06:00) Central Time (US & Canada)",
+  "(GMT-06:00) Guadalajara",
+  "(GMT-06:00) Mexico City",
+  "(GMT-06:00) Monterrey",
+  "(GMT-06:00) Saskatchewan",
+  "(GMT-05:00) Bogota",
+  "(GMT-05:00) Eastern Time (US & Canada)",
+  "(GMT-05:00) Indiana (East)",
+  "(GMT-05:00) Lima",
+  "(GMT-05:00) Quito",
+  "(GMT-04:00) Atlantic Time (Canada)",
+  "(GMT-04:00) Caracas",
+  "(GMT-04:00) La Paz",
+  "(GMT-04:00) Santiago",
+  "(GMT-03:30) Newfoundland",
+  "(GMT-03:00) Brasilia",
+  "(GMT-03:00) Buenos Aires",
+  "(GMT-03:00) Georgetown",
+  "(GMT-03:00) Greenland",
+  "(GMT-02:00) Mid-Atlantic",
+  "(GMT-01:00) Azores",
+  "(GMT-01:00) Cape Verde Is.",
+  "(GMT+00:00) Casablanca",
+  "(GMT+00:00) Dublin",
+  "(GMT+00:00) Edinburgh",
+  "(GMT+00:00) Lisbon",
+  "(GMT+00:00) London",
+];
 
 const currencies = [
-  'USD',
-  'EUR',
-  'GBP',
-  'AUD',
-  'BRL',
-  'CAD',
-  'CNY',
-  'CZK',
-  'DKK',
-  'HKD',
-  'HUF',
-  'INR',
-]
+  "USD",
+  "EUR",
+  "GBP",
+  "AUD",
+  "BRL",
+  "CAD",
+  "CNY",
+  "CZK",
+  "DKK",
+  "HKD",
+  "HUF",
+  "INR",
+];
 </script>
 
 <template>
@@ -139,15 +152,8 @@ const currencies = [
           <!-- 👉 Upload Photo -->
           <form class="d-flex flex-column justify-center gap-4">
             <div class="d-flex flex-wrap gap-4">
-              <VBtn
-                color="primary"
-                size="small"
-                @click="refInputEl?.click()"
-              >
-                <VIcon
-                  icon="tabler-cloud-upload"
-                  class="d-sm-none"
-                />
+              <VBtn color="primary" size="small" @click="refInputEl?.click()">
+                <VIcon icon="tabler-cloud-upload" class="d-sm-none" />
                 <span class="d-none d-sm-block">Upload new photo</span>
               </VBtn>
 
@@ -158,7 +164,7 @@ const currencies = [
                 accept=".jpeg,.png,.jpg,GIF"
                 hidden
                 @input="changeAvatar"
-              >
+              />
 
               <VBtn
                 type="reset"
@@ -168,10 +174,7 @@ const currencies = [
                 @click="resetAvatar"
               >
                 <span class="d-none d-sm-block">Reset</span>
-                <VIcon
-                  icon="tabler-refresh"
-                  class="d-sm-none"
-                />
+                <VIcon icon="tabler-refresh" class="d-sm-none" />
               </VBtn>
             </div>
 
@@ -187,14 +190,15 @@ const currencies = [
             <VRow>
               <!-- 👉 First Name -->
               <VCol md="6" cols="12">
-                <AppTextField v-model="accountDataLocal.firstName" placeholder="John" label="First Name" />
+                <AppTextField
+                  v-model="accountDataLocal.firstName"
+                  placeholder="John"
+                  label="First Name"
+                />
               </VCol>
 
               <!-- 👉 Last Name -->
-              <VCol
-                md="6"
-                cols="12"
-              >
+              <VCol md="6" cols="12">
                 <AppTextField
                   v-model="accountDataLocal.lastName"
                   placeholder="Doe"
@@ -204,14 +208,16 @@ const currencies = [
 
               <!-- 👉 Email -->
               <VCol cols="12" md="6">
-                <AppTextField v-model="accountDataLocal.email" label="E-mail" placeholder="johndoe@gmail.com" type="email" />
+                <AppTextField
+                  v-model="accountDataLocal.email"
+                  label="E-mail"
+                  placeholder="johndoe@gmail.com"
+                  type="email"
+                />
               </VCol>
 
               <!-- 👉 Organization -->
-              <VCol
-                cols="12"
-                md="6"
-              >
+              <VCol cols="12" md="6">
                 <AppTextField
                   v-model="accountDataLocal.org"
                   label="Organization"
@@ -220,10 +226,7 @@ const currencies = [
               </VCol>
 
               <!-- 👉 Phone -->
-              <VCol
-                cols="12"
-                md="6"
-              >
+              <VCol cols="12" md="6">
                 <AppTextField
                   v-model="accountDataLocal.phone"
                   label="Phone Number"
@@ -232,10 +235,7 @@ const currencies = [
               </VCol>
 
               <!-- 👉 Address -->
-              <VCol
-                cols="12"
-                md="6"
-              >
+              <VCol cols="12" md="6">
                 <AppTextField
                   v-model="accountDataLocal.address"
                   label="Address"
@@ -244,10 +244,7 @@ const currencies = [
               </VCol>
 
               <!-- 👉 State -->
-              <VCol
-                cols="12"
-                md="6"
-              >
+              <VCol cols="12" md="6">
                 <AppTextField
                   v-model="accountDataLocal.state"
                   label="State"
@@ -256,10 +253,7 @@ const currencies = [
               </VCol>
 
               <!-- 👉 Zip Code -->
-              <VCol
-                cols="12"
-                md="6"
-              >
+              <VCol cols="12" md="6">
                 <AppTextField
                   v-model="accountDataLocal.zip"
                   label="Zip Code"
@@ -268,10 +262,7 @@ const currencies = [
               </VCol>
 
               <!-- 👉 Country -->
-              <VCol
-                cols="12"
-                md="6"
-              >
+              <VCol cols="12" md="6">
                 <AppSelect
                   v-model="accountDataLocal.country"
                   label="Country"
@@ -281,10 +272,7 @@ const currencies = [
               </VCol>
 
               <!-- 👉 Language -->
-              <VCol
-                cols="12"
-                md="6"
-              >
+              <VCol cols="12" md="6">
                 <AppSelect
                   v-model="accountDataLocal.language"
                   label="Language"
@@ -294,10 +282,7 @@ const currencies = [
               </VCol>
 
               <!-- 👉 Timezone -->
-              <VCol
-                cols="12"
-                md="6"
-              >
+              <VCol cols="12" md="6">
                 <AppSelect
                   v-model="accountDataLocal.timezone"
                   label="Timezone"
@@ -308,10 +293,7 @@ const currencies = [
               </VCol>
 
               <!-- 👉 Currency -->
-              <VCol
-                cols="12"
-                md="6"
-              >
+              <VCol cols="12" md="6">
                 <AppSelect
                   v-model="accountDataLocal.currency"
                   label="Currency"
@@ -322,10 +304,7 @@ const currencies = [
               </VCol>
 
               <!-- 👉 Form Actions -->
-              <VCol
-                cols="12"
-                class="d-flex flex-wrap gap-4"
-              >
+              <VCol cols="12" class="d-flex flex-wrap gap-4">
                 <VBtn @click="saveProfile">Save changes</VBtn>
 
                 <VBtn
